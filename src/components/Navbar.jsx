d8af217647151006ae28867329bb26c964ec7577
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2023-12-28 17:38:17 +0200

    toast added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,18 @@ import { useDispatch, useSelector } from 'react-redux';
 import { selectIsLogged } from 'store/auth/selector';
 import { NavLink } from 'react-router-dom';
 import { logoutThunk } from 'store/auth/operations';
+import { toast } from 'react-toastify';
 
 const Navbar = () => {
   const isLogged = useSelector(selectIsLogged);
   const dispatch = useDispatch();
+
+  const logout = () =>
+    dispatch(logoutThunk())
+      .unwrap()
+      .then(res => {
+        toast.success(`See you!`);
+      });
   return (
     <div className="text-white font-bold flex gap-4">
       <NavLink to="/">Home</NavLink>
@@ -18,7 +26,7 @@ const Navbar = () => {
       )}
       {isLogged && (
         <>
-          |<button onClick={() => dispatch(logoutThunk())}>SignOut</button>
+          |<button onClick={() => logout()}>SignOut</button>
         </>
       )}
     </div>
